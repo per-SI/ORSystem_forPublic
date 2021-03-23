b287adf61361698ad6a72dc064e10ae4df05c741
Satoi Inagaki <per315ing@gmail.com>
2021-03-23 15:45:13 +0900

    add setting .gitignore

--- public/js/loading.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    const $loadingCover = document.getElementById('loadingCover');
-    const $spinner = document.getElementById('spinner');
-    const $loadingMessage = document.getElementById('loadingMessage');
-    $spinner.classList.remove('spinner');
-    $loadingCover.classList.add('loaded');
-    $loadingMessage.textContent="Loaded!";
-}
